dd2e8782b5b2923f4119e681ebb929c6e717a6c1
Edmilton Santos <EDMILTONSNTS@GMAIL.COM>
2024-11-04 11:57:28 -0300

    LOOPS

--- JAVASCRIPT/Laços/script.js
+++ JAVASCRIPT/Laços/script.js
@@ -1,33 +1,60 @@
 const carros = ["Gol", "Corsa", "Uno", "Punto", "Elantra"];
 
-
 const conteudo = document.getElementById("conteudo");
+ var dados;
+ var i;
 
+function criaSecao(titulo, dados) 
+{
+  let secao = document.createElement("div");
+  secao.innerHTML = "<h2>"+ titulo +"</h2>"+dados;
+  conteudo.appendChild(secao);
 
-let dados = "";
-let i = 0;
+}
+
+dados = "";
+i = 0;
 
 while(i < 4)
 {
 dados += "<P>"+carros[i]+"</P>";
   i++;
 }
+criaSecao("Loop while",dados)
 
-const secao = document.createElement("div");
-secao.innerHTML = "<h2>Loop while</h2>"+dados;
-conteudo.appendChild(secao);
+dados = "";
+i = 0;
 
-
-
-let dados2 = "";
-let i2 = 0;
-
-while(i2 < carros.length)
+while(i < carros.length)
 {
-dados2 += "<P>"+carros[i2]+"</P>";
-  i2++;
+dados += "<P>"+carros[i]+"</P>";
+  i++;
 }
 
-const secao2 = document.createElement("div");
-secao2.innerHTML = "<h2>Loop while melhorado</h2>"+dados2;
-conteudo.appendChild(secao2);
+criaSecao("Loop while melhorado",dados);
+
+dados = "";
+i = 0;
+
+do{
+  dados += "<P>"+carros[i]+"</P>";
+  i++;
+
+} while(i < carros.length)
+  criaSecao("Loop while invertido",dados);
+
+dados = "";
+
+
+for (i = 0; i < carros.length; i++) {
+  dados += "<P>"+carros[i]+"</P>";
+}
+criaSecao("Loop for",dados);
+
+
+dados = "";
+for(let carro of carros);
+{
+  dados += "<P>"+carro+"</P>";
+}
+criaSecao("Loop for OF",dados);
