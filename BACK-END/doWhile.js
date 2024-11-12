003a34b9db2ee3bfac28e4e5cf7c820d25c0185a
Edmilton Santos <edmiltonsnts@gmail.com>
2024-11-12 15:57:45 -0300

    POO

--- BACK-END/doWhile.js
+++ BACK-END/doWhile.js
@@ -1,14 +1,24 @@
-let count = 0;
+/*let count = 0;
 
-/*while(count < 10) {
+while(count < 10) {
   console.log(count);
   count++;
 }
 
-*/
-
 do {
-  console.log(count);
-  count++;
+ console.log(count);
+ count++;
 } while(count < 11)
-
+*/
+class carro {
+  constructor(marca) {
+    this.marca = marca; //propriedade da classe
+  }
+  mostraMarca()
+  {
+    console.log(`A marca do carro é ${this.marca}.`);
+  }
+}
+  
+const meuCarro = new carro("FORD");
+meuCarro.mostraMarca();
